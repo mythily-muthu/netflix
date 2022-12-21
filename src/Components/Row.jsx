00f43f24c4c9c8689b5a178dcaf8560244b51f28
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-21 19:43:54 +0530

    feat: added icons

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 let Row = ({ title, fetchURL }) => {
   let [movies, setMovies] = useState([]);
+  let [like, setLike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -21,11 +23,22 @@ let Row = ({ title, fetchURL }) => {
           {movies?.map((item) => (
             <div className=" w-[300px] sm:w-[200px] md:w-[250px] lg:w-[350px] inline-block cursor-pointer relative p-2">
               <img
+                className=" w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
                 alt={item?.title}
               />
-              <div className="absolute top-0  w-full h-full  flex items-center justify-center text-white text-md lg:text-xl hover:bg-black/50">
-                <p>{item?.title}</p>
+              <div>
+                <p className="absolute top-0  w-full h-full  flex items-center justify-center text-white text-md lg:text-xl hover:bg-black/50">
+                  {item?.title}
+                </p>
+
+                <p>
+                  {like ? (
+                    <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                  ) : (
+                    <FaRegHeart className="absolute top-4 right-5 text-red-600" />
+                  )}
+                </p>
               </div>
             </div>
           ))}
