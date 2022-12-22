e31209f95d9ac87ebb43bb0f6297eda2a514131d
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-22 12:26:26 +0530

    Feat: Added gradient to main component

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-const Movie = () => {
+const Movie = ({ item, id }) => {
+  let [like, setLike] = useState(false);
+
   return (
-    <div>
-      <div className=" w-[300px] sm:w-[200px] md:w-[250px] lg:w-[350px] inline-block cursor-pointer relative p-2">
-        <img
-          className=" w-full h-auto block"
-          src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
-          alt={item?.title}
-        />
-        <div>
-          <p className="absolute top-0  w-full h-full  flex items-center justify-center text-white text-md lg:text-xl hover:bg-black/50">
-            {item?.title}
-          </p>
+    <div className=" w-[300px] sm:w-[200px] md:w-[250px] lg:w-[350px] inline-block cursor-pointer relative p-2">
+      <img
+        className=" w-full h-auto block"
+        src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
+        alt={item?.title}
+      />
+      <div>
+        <p className="absolute top-0  w-full h-full  flex items-center justify-center text-white text-md lg:text-xl hover:bg-black/50">
+          {item?.title}
+        </p>
 
-          <p>
-            {like ? (
-              <FaHeart className="absolute top-4 left-4 text-gray-300" />
-            ) : (
-              <FaRegHeart className="absolute top-4 right-5 text-red-600" />
-            )}
-          </p>
-        </div>
+        <p>
+          {like ? (
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
+          ) : (
+            <FaRegHeart className="absolute top-4 right-5 text-red-600" />
+          )}
+        </p>
       </div>
     </div>
   );
