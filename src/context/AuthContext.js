d1b4d43791d9cc16e7ca4cddaada0906187c4c85
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-23 18:57:35 +0530

    Feat: added auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
@@ -15,7 +15,9 @@ export function AuthContextProvider({ children }) {
 
     function signUp(email, password) {
         createUserWithEmailAndPassword(auth, email, password);
-
+        setDoc(doc(db, 'users', email), {
+            savedShows: []
+        })
     }
 
     function logIn(email, password) {
