81915a2238459082f7d3074611f4ac2fe2304612
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-23 14:58:06 +0530

    feat: added the routes for login pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Home from './Pages/Home';
 
 import Navbar from './Components/Navbar';
 import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import Account from './Pages/Account';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
       </Routes>
     </>
   );
