39b115ace75f76fd8ea5ac50480fa6f42801ba20
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-23 14:46:07 +0530

    feat: added the auth context component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between p-6 absolute w-full z-[100]">
-      <h1 className="text-red-600 text-4xl font-bold flex">NETFLIX</h1>
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold  cursor-pointer flex">
+          NETFLIX
+        </h1>
+      </Link>
+
       <div className="">
-        <button className="text-white cursor-pointer p-3 font-bold">
-          Sign In
-        </button>
-        <button className="text-white bg-red-600 rounded px-5 py-2 cursor-pointer font-bold">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="text-white cursor-pointer p-3 font-bold">
+            Sign In
+          </button>
+        </Link>
+
+        <Link to="/signup">
+          <button className="text-white bg-red-600 rounded px-5 py-2 cursor-pointer font-bold">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,10 +10,10 @@ const firebaseConfig = {
     authDomain: process.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: process.REACT_APP_FIREBASE_MESSAGING_SENDER,
     appId: process.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
